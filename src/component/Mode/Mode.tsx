0401a88600fdf0563e6b2d85b8a4a561f147a712
maximus0509 <maximus0509@gmail.com>
2022-04-06 10:13:43 -0400

    working

--- src/component/Mode/Mode.tsx
+++ src/component/Mode/Mode.tsx
@@ -1,14 +1,28 @@
+import { IconLookup } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
+interface SelectModeProp {
+    onChangeMode?: Function
+}
+
 export class Mode extends React.Component {
+
+
     // onClick: Function;
-    constructor(props: any) {
+    constructor(props: SelectModeProp) {
         super(props);
         // this.onClick = this.onClick.bind(this)
     };
 
     render() {
-        return <div></div>
+        const barsIcon: IconLookup = { prefix: 'fas', iconName: 'bars' }
+
+        return <div className="ChangeModeButton">
+            <button className="btn">
+                <FontAwesomeIcon icon={barsIcon} />
+            </button>
+        </div>
         // return <button className="ChangeModeButton" onClick={this.onClick()}></button>
     };
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas)
 
 ReactDOM.render(
   <React.StrictMode>
